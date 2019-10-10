897b406dedfd08be87a8d29390cf58bc7c96b0ae
yisi-xgd <xss20427@sina.cn>
2019-10-10 14:30:38 +0800

    等待加入shiro与rbac

---
+++ src/main/java/com/wz/xgd/yisitest/util/MPGener.java
@@ -0,0 +1,65 @@
+package com.wz.xgd.yisitest.util;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+/**
+ * @author：wzxgd
+ * @date：10/10/2019 --------------
+ */
+public class MPGener {
+    public static void main(String[] args) {
+        //1. 全局配置
+        GlobalConfig config = new GlobalConfig();
+        config.setActiveRecord(false) // 是否支持AR模式
+                .setAuthor("yisi") // 作者
+                .setOutputDir("E:\\mp") // 生成路径
+                .setFileOverride(true)  // 文件覆盖
+                .setIdType(IdType.AUTO) // 主键策略
+                .setServiceName("%sService")  // 设置生成的service接口的名字的首字母是否为I
+                // IUserService
+                .setBaseResultMap(true)
+                .setBaseColumnList(true);
+
+        //2. 数据源配置
+        DataSourceConfig dsConfig = new DataSourceConfig();
+        dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
+                .setDriverName("com.mysql.jdbc.Driver")
+                .setUrl("jdbc:mysql://39.108.184.79:3306/yisi?useSSL=false&serverTimezone=GMT%2B8&characterEncoding")
+                .setUsername("yisiadmin")
+                .setPassword("wzyisi");
+
+        //3. 策略配置
+        StrategyConfig stConfig = new StrategyConfig();
+        stConfig.setCapitalMode(true) //全局大写命名
+                .setDbColumnUnderline(true)  // 指定表名 字段名是否使用下划线
+                .setNaming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
+                .setTablePrefix("tb_")
+                .setInclude("tb_user");  // 生成的表
+
+        //4. 包名策略配置
+        PackageConfig pkConfig = new PackageConfig();
+        pkConfig.setParent("com.wz.xgd.yisitest")
+                .setMapper("mapper")
+                .setService("service")
+                .setController("controller")
+                .setEntity("entity")
+                .setXml("mapper");
+
+        //5. 整合配置
+        AutoGenerator ag = new AutoGenerator();
+        ag.setGlobalConfig(config)
+                .setDataSource(dsConfig)
+                .setStrategy(stConfig)
+                .setPackageInfo(pkConfig);
+
+        //6. 执行
+        ag.execute();
+    }
+}
